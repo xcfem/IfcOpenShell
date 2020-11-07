df3636553deb277e5b4fcfb0cc7df05f803f411a
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-11-07 17:40:38 +0100

    Make CMake calling for Ubuntu 18 easier

---
+++ cmake/ubuntu_1804_cmake_command.sh
@@ -0,0 +1,7 @@
+cmake ../../IfcOpenShell/cmake/ -DOCC_LIBRARY_DIR="/usr/lib/x86_64-linux-gnu/" \
+      -DOPENCOLLADA_INCLUDE_DIR="/usr/local/include/opencollada" \
+      -DOPENCOLLADA_LIBRARY_DIR="/usr/local/lib/opencollada"  \
+      -DPCRE_LIBRARY_DIR="/usr/lib/x86_64-linux-gnu/" \
+      -DOCC_INCLUDE_DIR="/usr/local/include/oce"\
+      -DOCC_LIBRARY_DIR="/usr/local/lib" \
+      -DIFCXML_SUPPORT=Off
