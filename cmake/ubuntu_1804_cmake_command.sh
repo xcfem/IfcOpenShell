0da8de88b7bd7ce7c549baaed0f75acd681618ee
Luis C. Pérez Tato <l.pereztato@gmail.com>
2020-11-08 12:36:24 +0100

    Fix error in folder name

--- cmake/ubuntu_1804_cmake_command.sh
+++ cmake/ubuntu_1804_cmake_command.sh
@@ -1,4 +1,4 @@
-cmake ../../IfcOpenShell/cmake/ -DOCC_LIBRARY_DIR="/usr/lib/x86_64-linux-gnu/" \
+cmake ../../xcfem_ifcopenshell/cmake/ -DOCC_LIBRARY_DIR="/usr/lib/x86_64-linux-gnu/" \
       -DOPENCOLLADA_INCLUDE_DIR="/usr/local/include/opencollada" \
       -DOPENCOLLADA_LIBRARY_DIR="/usr/local/lib/opencollada"  \
       -DPCRE_LIBRARY_DIR="/usr/lib/x86_64-linux-gnu/" \
